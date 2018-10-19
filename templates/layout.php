2e7eaaf0dfbe22955e18e306aa62598b0b854bd0
Philip Maaß <PhilipMaasz@aol.com>
2018-10-19 18:35:10 +0200

    added content to layout template

--- templates/layout.php
+++ templates/layout.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link href="/public/assets/css/style.css" rel="stylesheet">
 
+    <meta charset = "UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+
+    <title>Example Title</title>
 </head>
 <body>
+
+<?php include $contents ?>
 
 </body>
 </html>
