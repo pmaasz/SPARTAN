e241d07a649fa09a979e7ac3a85a85ccac582b52
Philip Maaß <PhilipMaasz@aol.com>
2018-10-19 19:41:26 +0200

    Example has expalanation

--- Entity/Example.php
+++ Entity/Example.php
@@ -6,6 +6,13 @@
  * Time: 20:15
  */
 
+/**
+ * Class Example
+ *
+ * This object is an entity which reflects a data-model in our database. It consists of attributes, getter- and setter-
+ * functions.
+ */
+
 class Example
 {
     /**
