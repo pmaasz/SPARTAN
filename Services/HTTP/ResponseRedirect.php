0f02f9d6f37f0a9232f56264c42245f7c747304b
Philip Maaß <PhilipMaasz@aol.com>
2018-10-19 19:15:04 +0200

    ResponseRedirect has expalanation

--- Services/HTTP/ResponseRedirect.php
+++ Services/HTTP/ResponseRedirect.php
@@ -6,6 +6,14 @@
  * Time: 20:15
  */
 
+/**
+ * Class ResponseRedirect
+ *
+ * The ResponseRedirect does the same as the Response object. The difference is the location the user is directed to.
+ * Instead of loading the same page again we can redirect to a new page. Example would be the saving of a form and
+ * redirecting to the page where the saved data is shown.
+ */
+
 class ResponseRedirect implements ResponseInterface
 {
     /**
