0465b38dd7ffdda406fe06c019d59a60db8a3ee1
Philip Maaß <PhilipMaasz@aol.com>
2018-10-19 19:21:01 +0200

    added annotations in Singleton

--- Services/Singleton.php
+++ Services/Singleton.php
@@ -11,7 +11,7 @@
  *
  * The trait Singleton can also be implemented as a pattern but in this particular case we use it as a trait. All it
  * does is preventing us from building thousands of instances from our objects when we only need one. You can find it
- * in different classes mainly service classes. 
+ * in different classes mainly service classes.
  */
 
 trait Singleton
@@ -25,10 +25,16 @@ trait Singleton
     {
     }
 
+    /**
+     * clones Object we want to use when calling the instance
+     */
     protected function __clone()
     {
     }
 
+    /**
+     * @return mixed 
+     */
     public static function getInstance()
     {
         if( ! self::$instance )
