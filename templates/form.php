a096a07fcd5b9662ac648ee9a9aa4ac666cdedc5
Philip Maaß <PhilipMaasz@aol.com>
2018-10-19 18:31:30 +0200

    added content to form.php template

--- templates/form.php
+++ templates/form.php
@@ -1,9 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head>
+<div class="container">
+    <div class="card shadow">
+        <form name="userFrom" action="index.php?controller=UserController&action=createAction" method="POST">
+            <div class="card-body">
+                <div class="form-group row">
+                    <label for="attribute" class="form-control-label col-6">Attribute</label>
+                    <div class="col-6">
+                        <input type="text" name="attribute" value="">
+                    </div>
+                </div>
+            </div>
 
-</head>
-<body>
-
-</body>
-</html>
+            <div class="card-footer text-right">
+                <button type="submit" name="action" value="speichern">Save</button>
+                <a href="index.php?controller=UserController&action=indexAction">Cancel</a>
+            </div>
+        </form>
+    </div>
+</div>
