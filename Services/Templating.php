42e6265ca85ceaaa4a07b5a5e6c293413115cf6e
Philip Maaß <PhilipMaasz@aol.com>
2018-10-19 19:24:42 +0200

    added explanation for backToIndex function

--- Services/Templating.php
+++ Services/Templating.php
@@ -6,6 +6,10 @@
  * Time: 20:15
  */
 
+/**
+ *
+ */
+
 require_once 'Singleton.php';
 
 class Templating
@@ -15,13 +19,15 @@ class Templating
     /**
      * @param $template
      * @param array $parameters
-     * 
+     *
      * @return string
      */
     public function render($template, array $parameters = array())
     {
         ob_start();
+
         extract($parameters);
+
         include __DIR__ . '/../templates/' . $template;
 
         $content = ob_get_clean();
@@ -30,12 +36,13 @@ class Templating
     }
 
     /**
-     *bad code
+     *bad code because of exit function.
+     * we can replicate the same functionality by redirecting to our index.php in public.
+     * There we use our default Controller and default action.
      */
     public function backToIndex()
     {
         header('Location:');
         exit;
     }
-
 }
