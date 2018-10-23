c0d230222d7dc378b661978d95a4a8b5e8e58dd0
Philip Maaß <PhilipMaasz@aol.com>
2018-10-23 19:09:59 +0200

    Session explained in index.php

--- public/index.php
+++ public/index.php
@@ -11,6 +11,13 @@
  * routed to the correct classes and functions. Also the response given by the server to the client is send from here.
  */
 
+
+/**
+ * The Session is a global variable (array). This means you can access this array everywhere in your code via the
+ * $_SESSION call. Those variables are also called superglobal variables. The session is also using a
+ * Singleton-Pattern|Trait. Calling the function session_start() creates a new Session object or uses an already
+ * existing one. The Session is time limited an 'dissolves' after a set period of time.
+ */
 session_start();
 
 require_once __DIR__ . '/../Services/HTTP/Request.php';
