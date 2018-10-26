c7eceae9fc4ce4157fe7261a75575cada8e45517
Philip Maaß <PhilipMaasz@aol.com>
2018-10-26 18:16:02 +0200

    form shows value of data if it you click on update

--- templates/form.php
+++ templates/form.php
@@ -1,6 +1,6 @@
 <form name="userFrom" action="index.php?controller=ExampleController&action=createAction" method="POST">
     <label for="attribute" class="form-control-label col-6">Attribute: </label>
-    <input type="text" name="attribute" title="attribute" value="<?php if($data){$data->getAttribute();}?>">
+    <input type="text" name="attribute" title="attribute" value="<?= $data->getAttribute();?>">
 
     <button type="submit" name="action" value="speichern" class="saveButton">Save</button>
     <a href="index.php?controller=ExampleController&action=indexAction" class="cancelButton">Cancel</a>
