ef2e46d75b3ee4eb8a23ebc2effb6bb735c78d0c
Philip Maaß <PhilipMaasz@aol.com>
2022-03-27 03:10:58 +0200

    adds base entity

--- src/Entity/BaseEntity.php
+++ src/Entity/BaseEntity.php
@@ -11,5 +11,24 @@ namespace Entity;
 
 class BaseEntity
 {
+    /**
+     * @var int
+     */
+    private $id;
 
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 }
